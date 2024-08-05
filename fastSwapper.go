e50d0364eaf8e765c494893a6f21131909c86e2f
Lukas Bentele <bentele.l@googlemail.com>
2024-08-05 22:59:54 +0200

    Secondary commit

--- fastSwapper.go
+++ fastSwapper.go
@@ -1,1 +1,29 @@
-// test
+package main
+
+import ("fmt"
+        "os" 
+        "path/filepath"
+        // "log"
+        )
+
+
+func main() {
+ 
+  fmt.Println(getDirsInDir("~/Documents/go_testing/fastSwapper/testFolder"))
+
+  fmt.Println("Hello World!")
+}
+
+
+func getDirsInDir(dir string) [2]string{
+  entries, err := os.ReadDir(filepath.FromSlash(dir))
+  if err != nil{
+    //log.Fatal(err)
+    fmt.Println("error occured")
+  }
+  
+  for _, e := range entries{
+    fmt.Println(e.Name())
+  }
+  return [2]string{"a", "z"}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module fastSwapper
+
+go 1.22.5
