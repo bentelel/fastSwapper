6f18304bd5c83ca02d9d253ce14a1a83e537394c
AzureAD\LukasBentele <lukas.bentele@drjve.de>
2024-08-06 12:18:11 +0200

    made GetDirsInDir function functional

--- fastSwapper.go
+++ fastSwapper.go
@@ -3,27 +3,59 @@ package main
 import ("fmt"
         "os" 
         "path/filepath"
-        // "log"
+        "log"
         )
 
 
 func main() {
- 
-  fmt.Println(getDirsInDir("~/Documents/go_testing/fastSwapper/testFolder"))
 
-  fmt.Println("Hello World!")
+  dirs := getDirsInDir(`C:\go_testing\testFolder`)
+  allFiles := getAllInDir(`C:\go_testing\testFolder`)
+ 
+  fmt.Println("Printing all dirs:")
+  for _, e := range dirs{
+    fmt.Println(e)
+  }
+  fmt.Println("Printing all dirs and files:")
+  for _, e := range allFiles{
+    fmt.Println(e)
+  }
 }
 
 
-func getDirsInDir(dir string) [2]string{
+func getDirsInDir(dir string) []string{
+  // Returns slice of strings containing all directories within given directory
+  // param dir: string -- directory to check
   entries, err := os.ReadDir(filepath.FromSlash(dir))
+  fmt.Println(len(entries))
   if err != nil{
-    //log.Fatal(err)
+    log.Fatal(err)
     fmt.Println("error occured")
   }
-  
+  result := make([]string, 0) 
+  //translate all files to strings
   for _, e := range entries{
-    fmt.Println(e.Name())
+    if e.IsDir(){  
+      result = append(result,e.Name())
+    } 
   }
-  return [2]string{"a", "o"}
+  return result
 }
+
+func getAllInDir(dir string) []string{
+  // Returns slice of strings containing all directories within given directory
+  // param dir: string -- directory to check
+  entries, err := os.ReadDir(filepath.FromSlash(dir))
+  if err != nil{
+    log.Fatal(err)
+    fmt.Println("error occured")
+ }
+  result := make([]string, 0) 
+  //translate all files to strings
+  for _, e := range entries{
+    result = append(result,e.Name())
+  }
+  return result
+}
+
+
