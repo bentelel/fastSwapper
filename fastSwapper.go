6c2bf3cb93dc581de27c2c27ef94d3a1f7d5cd37
AzureAD\LukasBentele <lukas.bentele@drjve.de>
2024-08-07 12:05:02 +0200

    factored out some more general helper functions to lbtools.go

--- fastSwapper.go
+++ fastSwapper.go
@@ -7,8 +7,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"path/filepath"
-	"reflect"
 
 	"github.com/oleiade/reflections"
 )
@@ -35,20 +33,7 @@ func main() {
 
 	// setSettings("settings.json", "Tgkdir", "C:\\Tagetik\\Tagetik Excel .NET Client")
 
-	fmt.Println(getAllInDir(set.Tgkdir))
-}
-
-func typeof(v interface{}) string {
-	return reflect.TypeOf(v).String()
-}
-
-func containsString(s []string, e string) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
+	fmt.Println(GetAllInDir(set.Tgkdir))
 }
 
 func parseCLIargs(args []string) error {
@@ -58,13 +43,13 @@ func parseCLIargs(args []string) error {
 	}
 	// set default path flag expects the syntax of fastSwapper -d <path to directory>
 	const SET_DEFAULT_PATH_FLAG = "-d"
-	if containsString(args, SET_DEFAULT_PATH_FLAG) {
+	if ContainsString(args, SET_DEFAULT_PATH_FLAG) {
 		if len(args) < 2 {
 			err = errors.New("No path provided. Use fastSwapper -d <path to default dir>.")
 			return err
 		}
 		candidatePath := args[1]
-		if !isDir(candidatePath) {
+		if !IsDir(candidatePath) {
 			err = errors.New("Supplied path does not exist.")
 			return err
 		}
@@ -72,13 +57,6 @@ func parseCLIargs(args []string) error {
 	}
 
 	return err
-}
-
-func isDir(path string) bool {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return false
-	}
-	return true
 }
 
 func unmarshalSettingsJson(filename string) Settings {
@@ -122,36 +100,4 @@ func setSettings(filename string, defaultToChange string, newValue string) {
 		log.Fatal(err)
 	}
 	updateSettingsJson(filename, unmarshaledJson)
-}
-
-func getDirsInDir(dir string) []string {
-	// Returns slice of strings containing all directories within given directory
-	// param dir: string -- directory to check
-	entries, err := os.ReadDir(filepath.FromSlash(dir))
-	if err != nil {
-		log.Fatal(err)
-	}
-	result := make([]string, 0)
-	// translate all vfiles to strings
-	for _, e := range entries {
-		if e.IsDir() {
-			result = append(result, e.Name())
-		}
-	}
-	return result
-}
-
-func getAllInDir(dir string) []string {
-	// Returns slice of strings containing all directories within given directory
-	// param dir: string -- directory to check
-	entries, err := os.ReadDir(filepath.FromSlash(dir))
-	if err != nil {
-		log.Fatal(err)
-	}
-	result := make([]string, 0)
-	// translate all files to strings
-	for _, e := range entries {
-		result = append(result, e.Name())
-	}
-	return result
 }
---
+++ lbtools.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+)
+
+func Typeof(v interface{}) string {
+	return reflect.TypeOf(v).String()
+}
+
+func ContainsString(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func IsDir(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func GetDirsInDir(dir string) []string {
+	// Returns slice of strings containing all directories within given directory
+	// param dir: string -- directory to check
+	entries, err := os.ReadDir(filepath.FromSlash(dir))
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := make([]string, 0)
+	// translate all vfiles to strings
+	for _, e := range entries {
+		if e.IsDir() {
+			result = append(result, e.Name())
+		}
+	}
+	return result
+}
+
+func GetAllInDir(dir string) []string {
+	// Returns slice of strings containing all directories within given directory
+	// param dir: string -- directory to check
+	entries, err := os.ReadDir(filepath.FromSlash(dir))
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := make([]string, 0)
+	// translate all files to strings
+	for _, e := range entries {
+		result = append(result, e.Name())
+	}
+	return result
+}
